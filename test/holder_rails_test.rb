e656eec31ee834c309f619b3a5585f2dd8cb8f46
Ben Trewern <ben.trewern@gmail.com>
2017-01-30 02:04:10 +0000

    Update test to not expect src=""

--- test/holder_rails_test.rb
+++ test/holder_rails_test.rb
@@ -5,26 +5,26 @@ class HolderRailsTest < ActionView::TestCase
   include HolderRails::Helpers
 
   test "size" do
-    assert_dom_equal '<img data-src="holder.js/100x100?" src="" />', holder_tag(100)
-    assert_dom_equal '<img data-src="holder.js/200x300?" src="" />', holder_tag('200x300')
-    assert_dom_equal '<img data-src="holder.js/100px75?" src="" />', holder_tag('100px75')
+    assert_dom_equal '<img data-src="holder.js/100x100?" />', holder_tag(100)
+    assert_dom_equal '<img data-src="holder.js/200x300?" />', holder_tag('200x300')
+    assert_dom_equal '<img data-src="holder.js/100px75?" />', holder_tag('100px75')
   end
 
   test "text" do
-    assert_dom_equal '<img data-src="holder.js/200x300?text=Lorem ipsum" src="" />', holder_tag('200x300', 'Lorem ipsum')
+    assert_dom_equal '<img data-src="holder.js/200x300?text=Lorem ipsum" />', holder_tag('200x300', 'Lorem ipsum')
   end
 
   test "theme" do
-    assert_dom_equal '<img data-src="holder.js/200x300?text=Lorem ipsum&amp;theme=social" src="" />', holder_tag('200x300', 'Lorem ipsum', 'social')
+    assert_dom_equal '<img data-src="holder.js/200x300?text=Lorem ipsum&amp;theme=social" />', holder_tag('200x300', 'Lorem ipsum', 'social')
   end
 
   test "html_options" do
-    assert_dom_equal '<img class="special" data-src="holder.js/500x800?text=Example text&amp;theme=gray" id="new" src="" />',
+    assert_dom_equal '<img class="special" data-src="holder.js/500x800?text=Example text&amp;theme=gray" id="new" />',
       holder_tag('500x800', 'Example text', 'gray', id: 'new', class: 'special')
   end
 
   test "holder_options" do
-    assert_dom_equal '<img class="special" data-src="holder.js/500x800?font=Helvetica&amp;text=Example text&amp;theme=gray" id="new" src="" />',
+    assert_dom_equal '<img class="special" data-src="holder.js/500x800?font=Helvetica&amp;text=Example text&amp;theme=gray" id="new" />',
       holder_tag('500x800', 'Example text', 'gray', { id: 'new', class: 'special' }, { font: 'Helvetica' })
   end
 end
